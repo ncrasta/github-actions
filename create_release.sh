328a82188cc3f7ea2e23607e50a766e3f23763fc
Naveen Crasta <naveen.crasta@gmail.com>
2022-09-29 20:10:34 +0200

    upload

--- create_release.sh
+++ create_release.sh
@@ -3,7 +3,7 @@
 release_version=0.0.2
 release_name="v${release_version}"
 git add .
-git commit -m "Update release version"
+git commit -m "Update release version ${release_version}"
 git push origin
 git tag -a ${release_name} -m ${release_name}
 git push origin ${release_name}
