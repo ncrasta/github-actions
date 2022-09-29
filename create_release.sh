e6735b18fd3615586bebaad83e3aeb6505db9a32
Naveen Crasta <naveen.crasta@gmail.com>
2022-09-29 20:12:00 +0200

    Update release version 0.0.3

--- create_release.sh
+++ create_release.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-release_version=0.0.2
+release_version=0.0.3
 release_name="v${release_version}"
 git add .
 git commit -m "Update release version ${release_version}"
-git push origin
+git push
 git tag -a ${release_name} -m ${release_name}
 git push origin ${release_name}
